acc9c742614e42487d0a291c7b8c5b14fe469d83
evgp <bratikha@gmail.com>
2020-02-18 14:50:22 +0700

    minecraft module

---
+++ site/minecraft/manifests/init.pp
@@ -0,0 +1,30 @@
+ass minecraft (
+  $url = 'https://launcher.mojang.com/v1/objects/bb2b6b1aefcd70dfd1892149ac3a215f6c636b07/server.jar',
+  $install_dir = '/opt/minecraft'
+){
+  file { $install_dir:
+    ensure => directory,
+  }
+  file { "${install_dir}/minecraft_server.jar":
+    ensure => file,
+    source => $url,
+  }
+
+  package {'java':
+    ensure => present,
+  }
+  file {"${install_dir}/eula.txt":
+    ensure => file,
+    content => 'eula=true'
+  }
+  file {‘/etc/systemd/system/minecraft.service’:
+    ensure => file,
+    content => epp(‘minecraft/minecraft.service.epp’, {
+      install_dir => $install_dir,
+    }),
+  }
+  service { 'minecraft':
+    ensure => running,
+    enable => true,
+  }
+}
